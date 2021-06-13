357d63c91b49936ca7cd166a5329cefac5f1a8bd
Brandon Maxwell <brandon.l.maxwell@gmail.com>
2021-06-12 21:11:29 -0500

    updating heroku

--- public/db.js
+++ public/db.js
@@ -77,14 +77,12 @@ request.onsuccess = function (e) {
   }
 };
 
-const saveRecord = (record) => {
+function saveRecord(record) {
   console.log('Save record invoked');
   // Create a transaction on the BudgetStore db with readwrite access
   const transaction = db.transaction(['BudgetStore'], 'readwrite');
-
   // Access your BudgetStore object store
   const store = transaction.objectStore('BudgetStore');
-
   // Add record to your store with add method.
   store.add(record);
 };
